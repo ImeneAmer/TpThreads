146b966a0b1804c985adfba65a0fd4f453bcf715
IMENE <AMER@DESKTOP-89RMBLU.(none)>
2023-05-01 14:02:01 +0100

    first commit

---
+++ TpThread/src/Main.java
@@ -0,0 +1,17 @@
+import MyThread.Talkative;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i < 10; i++) {
+            Talkative talkative = new Talkative(i); // Création d'un nouvel objet Talkative avec un entier unique
+            Thread thread = new Thread(talkative); // Création d'un nouveau Thread avec l'objet Talkative comme paramètre
+            thread.setName("Thread " + i); // Attribution d'un nom unique pour chaque Thread
+            thread.start(); // Démarrage du Thread
+        }
+
+            }
+}
